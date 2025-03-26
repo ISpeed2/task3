931b7b976ae2fe8aea741673fda49c9ff24ffb41
ISpeed2 <bro228nur@gmail.com>
2025-03-26 09:02:59 +0300

    Update Bank.h

--- Bank.h
+++ Bank.h
@@ -3,15 +3,14 @@
 
 #include <string>
 #include <vector>
-#include <memory> // Для unique_ptr
+#include <memory>
 #include "Client.h"
 #include "Account.h"
 #include "Transaction.h"
 #include "Observer.h"
-#include "Exceptions.h" // Для исключений
-#include "TimeService.h" //  Для работы со временем
+#include "Exceptions.h"
+#include "TimeService.h"
 
-// Forward declaration
 class CentralBank;
 
 class Bank : public Subject {
@@ -20,7 +19,8 @@ public:
     ~Bank() override;
 
     void addClient(const Client& client);
-    std::unique_ptr<Account> createAccount(Client& client, AccountType accountType, double initialBalance);
+    std::unique_ptr<Account> createAccount(Client& client, AccountType accountType, double initialBalance,
+                                             double interestRate = 0.0, double creditLimit = 0.0, double fee = 0.0, long expirationTime = 0);
     void deposit(Account* account, double amount);
     bool withdraw(Account* account, double amount);
     bool transfer(Account* fromAccount, Account* toAccount, double amount);
@@ -38,12 +38,7 @@ private:
     std::string bank_id;
     std::vector<Client> clients;
     std::vector<std::unique_ptr<Account>> accounts;
-    //std::unique_ptr<InterestRateStrategy> interest_rate_strategy; // Пример Strategy
-
-    // Observer Pattern
     void notify() override;
-
-    // Friend class для доступа к приватным членам (если нужно)
     friend class CentralBank;
 };
 
